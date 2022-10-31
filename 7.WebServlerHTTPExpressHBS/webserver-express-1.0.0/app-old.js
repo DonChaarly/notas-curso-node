a9711804b0e1b050fef80d757a56e87bf24f4fe8
DonChaarly <carlos.ramosd43@gmail.com>
2022-10-31 16:56:33 -0600

    Agregados comentarios al proyecto seccion 7

--- 7.WebServlerHTTPExpressHBS/webserver-express-1.0.0/app-old.js
+++ 7.WebServlerHTTPExpressHBS/webserver-express-1.0.0/app-old.js
@@ -1,8 +1,13 @@
+//1. Para crear **servidores http** con solo node se utiliza la **libreria http**
 const http = require('http');
 
-
+/*2. Lo primero es crear un servidor, en este se estalece el puerto en el que se estara escuchando en el listen
+     Se tienen los parametros req y res, con req es lo que el usuario nos mandar y res para indicar que mandaremos
+ */
 http.createServer((req, res) => {
 
+        
+        //5. Con res.writeHead se colocan los cabeceros de repuesta que se van a mandar como parte de la peticion
         res.writeHead(200, { 'Content-Type': 'application/json' });
 
         let salida = {
@@ -11,8 +16,9 @@ http.createServer((req, res) => {
             url: req.url
         }
 
+        //3. Para mandar algo se coloca res.write
         res.write(JSON.stringify(salida));
-        // res.write('Hola Mundo');
+        //4. Se tiene que especificar el res.end() para indicar que ya se termino de escribir la respuesta -> server.js
         res.end();
 
     })
--- 7.WebServlerHTTPExpressHBS/webserver-express-1.0.0/server.js
+++ 7.WebServlerHTTPExpressHBS/webserver-express-1.0.0/server.js
@@ -1,33 +1,42 @@
+//5. Se hace la importacion de express y se crea una constante app
 const express = require('express');
 const app = express();
 
+// 9. Para utilizar las renderizaciones de handlebars se debe importar 
 const hbs = require('hbs');
 require('./hbs/helpers');
 
 const port = process.env.PORT || 3000;
 
-
+/*8. Para servir contenido estatico se debe especificar que carpeta sera publica 
+     esto se hace con el metodo **app.use(expres.static())**
+*/
 app.use(express.static(__dirname + '/public'));
 
-// Express HBS engine
+//10. Tambien se tiene que establecer la carpeta en donde se encontraran los archivo
 hbs.registerPartials(__dirname + '/views/parciales');
+//11. Se indica que se utilizara handlebars
 app.set('view engine', 'hbs');
 
 
 
-
+//7. Para establecer una ruta y lo que regresera el servidor con la misma se utilza app.get()
 app.get('/', (req, res) => {
 
+    
     res.render('home', {
         nombre: 'fernando'
     });
 });
 
 app.get('/about', (req, res) => {
-
+    //12. Para indicar que archivo se renderizara con handlebars se utilizara el metodo res.render()
     res.render('about');
 });
 
+/*6. Se establece abajo de todo el puerto en el que se estara escuchando con app.listen()
+     Se acostumbra crear una constante para definir el puerto y mandar un callback al listen()
+*/
 app.listen(port, () => {
     console.log(`Escuchando peticiones en el puerto ${ port }`);
 });
